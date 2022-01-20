5d2106aa4b832c54ce8c136802d04f7eeb29bca3
thiago <thiagohcalvi@gmail.com>
2022-01-20 13:53:52 -0300

    Introdução a C# e .NET

---
+++ Introdução ao c# e .NET/CSharp/CSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CSharp;
+using Interface;
+using Enum;
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var pessoa = (Pessoas)0;
+            Pessoas pessoa2 = Pessoas.João;
+            Pessoas pessoa3 = (Pessoas)4;
+
+
+
+            Pessoa person = new Pessoa();
+
+            person.Nome = "Thiago";
+            person.Estado = "Parana";
+            person.Idade = 17;
+
+            var person2 = new Pessoa();
+
+            person2.Nome = "Givanna";
+            person2.Estado = "São Paulo";
+            person2.Idade = 18;
+
+
+            Animal animal = new Animal();
+            animal.Nome = "Rex";
+            animal.Dono = "Pedro";
+            animal.Especie = "Cachorro";
+        }
+    }
+}
