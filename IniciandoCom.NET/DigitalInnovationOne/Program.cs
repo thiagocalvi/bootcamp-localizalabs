397a6af9c4edc3b0a155d156511515161f2891a2
thiago <thiagohcalvi@gmail.com>
2022-01-21 15:38:52 -0300

    Programa em c#

---
+++ IniciandoCom.NET/DigitalInnovationOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 10;
+            for(int i=1; i<numeroDeVezes; i++){
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
