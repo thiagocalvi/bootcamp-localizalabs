b28b7438de5b8485059fa33d0dda21aee7d89197
thiago <thiagohcalvi@gmail.com>
2022-02-03 11:07:23 -0300

    last commit

---
+++ Desing Patterns .NET/Localiza.Frotas.Infra/Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Localiza.Frotas.Infra.Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        public Guid Id { get; } = Guid.NewGuid();
+
+        private Singleton() 
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
---
+++ Desing Patterns .NET/Localiza.Frotas/Controllers/SingletonController.cs
@@ -0,0 +1,18 @@
+﻿using Localiza.Frotas.Infra.Singleton;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Localiza.Frotas.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SingletonController : ControllerBase
+    {
+        [HttpGet()]
+
+        public IActionResult Get()
+        {
+            var singleton = Singleton.Instance();
+            return Ok(singleton);
+        }
+    }
+}
